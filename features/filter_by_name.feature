cc42031512176829b2204004758bffb19f3ca7b3
Soumyajit Basu <99189547+soumyjitbasu@users.noreply.github.com>
2022-07-27 16:41:53 +0530

    Grouped the Test Steps into scenario in the report

--- features/filter_by_name.feature
+++ features/filter_by_name.feature
@@ -1,54 +1,56 @@
-Feature: Filter
+Feature: Test Scenarios
 
-    Scenario: P1 Test Cases
+    Scenario: Multiple Test Scenarios
+        #comment Soumyajit Test
         Given I open the website
 
         # SCENARIO 1 - POSITIVE SCENARIO - VALID SEARCH
-        When I enter the computer name "Acer Iconia"
-        And User clicks on filer by name button
-        Then one result is retuened with name Acer Iconia
+        When SCENARIO 1 - Valid Search - I enter the computer name "Acer Iconia"
+        And SCENARIO 1 - Valid Search - User clicks on filer by name button
+        Then SCENARIO 1 - Valid Search - one result is retuened with name Acer Iconia
 
         # SCENARIO 2 - NEGATIVE SCENARIO - INVALID SEARCH [RETURN NO RESULT]
-        And enter an invalid computer name "&*&*&*&"
-        And user clicks search
-        Then no result should be retruned
+        And SCENARIO 2 - Invalid Search - enter an invalid computer name "&*&*&*&"
+        And SCENARIO 2 - Invalid Search - user clicks search
+        Then SCENARIO 2 - Invalid Search - no result should be retruned
 
         # SCENARIO 3 - POSITIVE SCENARIO - ADD A COMPUTER [HAPPY FLOW]
-        And user clicks add new computer button
-        And enter a computer name "test_computer"
-        And enter Introduced Date "2000-06-02"
-        And enter Discontinued Date "2022-06-01"
-        And choose company
-        And create the computer
-        Then successful computer creation
+        And SCENARIO 3 - Computer Add - user clicks add new computer button
+        And SCENARIO 3 - Computer Add - enter a computer name "test_computer"
+        And SCENARIO 3 - Computer Add - enter Introduced Date "2000-06-02"
+        And SCENARIO 3 - Computer Add - enter Discontinued Date "2022-06-01"
+        And SCENARIO 3 - Computer Add - choose company
+        And SCENARIO 3 - Computer Add - create the computer
+        Then SCENARIO 3 - Computer Add - successful computer creation
 
         # SCENARIO 4 - NEGATIVE SCENARIO - ADD A COMPUTER WITH SPECIAL CHARACTERS - STLL GETS ADDED
-        And user clicks add new computer
-        And enter all special characters in computer name "!@#$%^"
-        And enter Introduced Valid Date "2000-06-02"
-        And enter Discontinued Valid Date "2022-06-01"
-        And choose valid company
-        And create the computer with blank name
-        Then successful computer creation with blank name
+        And SCENARIO 4 - Special Character in Computer Name - user clicks add new computer
+        And SCENARIO 4 - Special Character in Computer Name - enter all special characters in computer name "!@#$%^"
+        And SCENARIO 4 - Special Character in Computer Name - enter Introduced Valid Date "2000-06-02"
+        And SCENARIO 4 - Special Character in Computer Name - enter Discontinued Valid Date "2022-06-01"
+        And SCENARIO 4 - Special Character in Computer Name - choose valid company
+        And SCENARIO 4 - Special Character in Computer Name - create the computer with blank name
+        Then SCENARIO 4 - Special Character in Computer Name - successful computer creation with blank name
 
         # SCENARIO 5 - NEGATIVE SCENARIO - ADD COMPUTER WITH ONLY DATA, NOT OTHER DATA - STILL GETS ADDED
-        And user clicks add new computer
-        And enter all special characters in computer name "No Other Fields"
-        And create the computer with blank name
-        Then successful computer creation with blank name
+        And SCENARIO 5 - Negative Computer Add Scenario - user clicks add new computer
+        And SCENARIO 5 - Negative Computer Add Scenario - No computer name "No Other Fields"
+        And SCENARIO 5 - Negative Computer Add Scenario - create computer
+        Then SCENARIO 5 - Negative Computer Add Scenario - successful computer creation with blank name
 
         # SCENARIO 6 - NEGATIVE SCENARIO - ADD COMPUTER MANDATORY FILED VALIDATION
-        And go back to add computer page
-        And click add computer button
-        Then validation of computer name mandatory message validation
+        And SCENARIO 6 - Computer Mandatory Field Validation - go back to add computer page
+        And SCENARIO 6 - click add computer button
+        Then SCENARIO 6 - validation of computer name mandatory message validation
 
         # SCENARIO 7 - NEGATIVE SCENARIO - DATE FORMAT VALIDATION FOR Introduced and Dicsontinuted Date
-        And enter all special characters in computer name "Soumyajit Computer"
-        And enter Introduced Valid Date "2000-13-02"
-        And click add computer button1
+        And SCENARIO 7 - DATE FORMAT VALIDATION "Soumyajit Computer"
+        And SCENARIO 7 - DATE FORMAT VALIDATION enter Introduced Valid Date "2000-13-02"
+        And SCENARIO 7 - click add computer button1
         Then validation of incorrect date format
-        And enter Introduced Valid Date "2000-12-02"
-        And enter Discontinued Valid Date "2022-13-01"
-        And click add computer button2
+        And SCENARIO 7 - DATE FORMAT VALIDATION enter Introduced Valid Date "2000-12-02"
+        And SCENARIO 7 - DATE FORMAT VALIDATION Discontinued Valid Date "2022-13-01"
+        And SCENARIO 7 - click add computer button2
         Then validation of incorrect date format
+
 
--- features/step_definitions/filter_by_name_steps.js
+++ features/step_definitions/filter_by_name_steps.js
@@ -7,7 +7,10 @@ Given ('I open the website', async function(){
 
 });
 
-When ('I enter the computer name {string}', async function(computer_name){
+
+// SCENARIO 1
+
+When ('SCENARIO 1 - Valid Search - I enter the computer name {string}', async function(computer_name){
 
     let input = Selector('input#searchbox').with({boundTestRun:testController});
     await testController
@@ -15,7 +18,7 @@ When ('I enter the computer name {string}', async function(computer_name){
 
 });
 
-When ('User clicks on filer by name button', async function() {
+When ('SCENARIO 1 - Valid Search - User clicks on filer by name button', async function() {
 
     let button = Selector('input#searchsubmit').with({boundTestRun:testController});
     await testController
@@ -23,20 +26,21 @@ When ('User clicks on filer by name button', async function() {
     
 });
 
-Then ('one result is retuened with name Acer Iconia', async function(){
+Then ('SCENARIO 1 - Valid Search - one result is retuened with name Acer Iconia', async function(){
     let section = Selector('section#main').with({boundTestRun:testController});
     await testController.expect(section.innerText).contains('One computer found');
 });
 
 
-When ('enter an invalid computer name {string}', async function(invalid_computer_name){
+// SCENARIO 2
+When ('SCENARIO 2 - Invalid Search - enter an invalid computer name {string}', async function(invalid_computer_name){
 
     let input = Selector('input#searchbox').with({boundTestRun:testController});
     await testController
     .typeText(input,invalid_computer_name);
 });
 
-When ('user clicks search', async function() {
+When ('SCENARIO 2 - Invalid Search - user clicks search', async function() {
 
     let button = Selector('input#searchsubmit').with({boundTestRun:testController});
     await testController
@@ -44,38 +48,38 @@ When ('user clicks search', async function() {
     
 });
 
-Then ('no result should be retruned', async function(){
+Then ('SCENARIO 2 - Invalid Search - no result should be retruned', async function(){
     let section = Selector('section#main').with({boundTestRun:testController});
     await testController.expect(section.innerText).contains('No computers found');
 });
 
-
-When ('user clicks add new computer button', async function() {
+// SCENARIO 3
+When ('SCENARIO 3 - Computer Add - user clicks add new computer button', async function() {
 
     let add = Selector('a#add').with({boundTestRun:testController});
     await testController
     .click(add);
 });
 
-When ('enter a computer name {string}', async function(computer_name_valid){
+When ('SCENARIO 3 - Computer Add - enter a computer name {string}', async function(computer_name_valid){
     let computer_name = Selector('input#name').with({boundTestRun: testController});
     await testController
     .typeText(computer_name, computer_name_valid)
 });
 
-When ('enter Introduced Date {string}', async function(introduced_date){
+When ('SCENARIO 3 - Computer Add - enter Introduced Date {string}', async function(introduced_date){
     let intro_date = Selector('input#introduced').with({boundTestRun: testController});
     await testController
     .typeText(intro_date, introduced_date)
 });
 
-When ('enter Discontinued Date {string}', async function(discontinued_date){
+When ('SCENARIO 3 - Computer Add - enter Discontinued Date {string}', async function(discontinued_date){
     let disc_date = Selector('input#discontinued').with({boundTestRun: testController});
     await testController
     .typeText(disc_date, discontinued_date)
 });
 
-When ('choose company', async function(){
+When ('SCENARIO 3 - Computer Add - choose company', async function(){
     let company = Selector('select#company').with({boundTestRun: testController});
     let company_option = company.find('option');
     await testController
@@ -83,7 +87,7 @@ When ('choose company', async function(){
     .click(company_option.withText('RCA'))
 });
 
-When ('create the computer', async function() {
+When ('SCENARIO 3 - Computer Add - create the computer', async function() {
 
     await testController
     .pressKey("tab");
@@ -94,39 +98,39 @@ When ('create the computer', async function() {
  
 });
 
-Then ('successful computer creation', async function(){
+Then ('SCENARIO 3 - Computer Add - successful computer creation', async function(){
     let successmessage = Selector('div').child('strong').with({boundTestRun: testController})
     await testController.expect(successmessage.innerText).contains('Done!');
 });
 
 
-
-When ('user clicks add new computer', async function() {
+// SCENARIO 4
+When ('SCENARIO 4 - Special Character in Computer Name - user clicks add new computer', async function() {
 
     let add = Selector('a#add').with({boundTestRun:testController});
     await testController
     .click(add);
 });
 
-When ('enter all special characters in computer name {string}', async function(computer_name_valid){
+When ('SCENARIO 4 - Special Character in Computer Name - enter all special characters in computer name {string}', async function(computer_name_valid){
     let computer_name = Selector('input#name').with({boundTestRun: testController});
     await testController
     .typeText(computer_name, computer_name_valid)
 });
 
-When ('enter Introduced Valid Date {string}', async function(introduced_date){
+When ('SCENARIO 4 - Special Character in Computer Name - enter Introduced Valid Date {string}', async function(introduced_date){
     let intro_date = Selector('input#introduced').with({boundTestRun: testController});
     await testController
     .typeText(intro_date, introduced_date)
 });
 
-When ('enter Discontinued Valid Date {string}', async function(discontinued_date){
+When ('SCENARIO 4 - Special Character in Computer Name - enter Discontinued Valid Date {string}', async function(discontinued_date){
     let disc_date = Selector('input#discontinued').with({boundTestRun: testController});
     await testController
     .typeText(disc_date, discontinued_date)
 });
 
-When ('choose valid company', async function(){
+When ('SCENARIO 4 - Special Character in Computer Name - choose valid company', async function(){
     let company = Selector('select#company').with({boundTestRun: testController});
     let company_option = company.find('option');
     await testController
@@ -134,7 +138,7 @@ When ('choose valid company', async function(){
     .click(company_option.withText('RCA'))
 });
 
-When ('create the computer with blank name', async function() {
+When ('SCENARIO 4 - Special Character in Computer Name - create the computer with blank name', async function() {
 
     await testController
     .pressKey("tab");
@@ -145,19 +149,22 @@ When ('create the computer with blank name', async function() {
  
 });
 
-Then ('successful computer creation with blank name', async function(){
+
+Then ('SCENARIO 4 - Special Character in Computer Name - successful computer creation with blank name', async function(){
     let successmessage = Selector('div').child('strong').with({boundTestRun: testController})
     await testController.expect(successmessage.innerText).contains('Done!');
 });
 
-When ('go back to add computer page', async function() {
+
+// SCENARIO 6
+When ('SCENARIO 6 - Computer Mandatory Field Validation - go back to add computer page', async function() {
 
     let add = Selector('a#add').with({boundTestRun:testController});
     await testController
     .click(add);
 });
 
-When ('click add computer button', async function() {
+When ('SCENARIO 6 - click add computer button', async function() {
 
     await testController
     .pressKey("tab");
@@ -189,7 +196,7 @@ When ('click add computer button', async function() {
  
 });
 
-Then ('validation of computer name mandatory message validation', async function(){
+Then ('SCENARIO 6 - validation of computer name mandatory message validation', async function(){
     let validation_msg = Selector('span').with({boundTestRun: testController})
     await testController.expect(validation_msg.innerText).contains('Required');
 });
@@ -199,7 +206,7 @@ Then ('validation of incorrect date format', async function(){
     await testController.expect(date_filed.innerText).contains('Date');
 });
 
-When ('click add computer button1', async function() {
+When ('SCENARIO 7 - click add computer button1', async function() {
 
     await testController
     .pressKey("tab");
@@ -218,7 +225,7 @@ When ('click add computer button1', async function() {
  
 });
 
-When ('click add computer button2', async function() {
+When ('SCENARIO 7 - click add computer button2', async function() {
 
     await testController
     .pressKey("tab");
@@ -229,4 +236,51 @@ When ('click add computer button2', async function() {
     await testController
     .pressKey("enter");
  
+});
+
+When ('SCENARIO 5 - Negative Computer Add Scenario - user clicks add new computer', async function() {
+
+    let add = Selector('a#add').with({boundTestRun:testController});
+    await testController
+    .click(add);
+});
+
+When ('SCENARIO 5 - Negative Computer Add Scenario - No computer name {string}', async function(computer_name_valid){
+    let computer_name = Selector('input#name').with({boundTestRun: testController});
+    await testController
+    .typeText(computer_name, computer_name_valid)
+});
+
+When ('SCENARIO 5 - Negative Computer Add Scenario - create computer', async function() {
+
+    await testController
+    .pressKey("tab");
+    await testController
+    .wait(2000);
+    await testController
+    .pressKey("enter");
+ 
+});
+
+Then ('SCENARIO 5 - Negative Computer Add Scenario - successful computer creation with blank name', async function(){
+    let successmessage = Selector('div').child('strong').with({boundTestRun: testController})
+    await testController.expect(successmessage.innerText).contains('Done!');
+});
+
+When ('SCENARIO 7 - DATE FORMAT VALIDATION {string}', async function(computer_name_valid){
+    let computer_name = Selector('input#name').with({boundTestRun: testController});
+    await testController
+    .typeText(computer_name, computer_name_valid)
+});
+
+When ('SCENARIO 7 - DATE FORMAT VALIDATION enter Introduced Valid Date {string}', async function(introduced_date){
+    let intro_date = Selector('input#introduced').with({boundTestRun: testController});
+    await testController
+    .typeText(intro_date, introduced_date)
+});
+
+When ('SCENARIO 7 - DATE FORMAT VALIDATION Discontinued Valid Date {string}', async function(discontinued_date){
+    let disc_date = Selector('input#discontinued').with({boundTestRun: testController});
+    await testController
+    .typeText(disc_date, discontinued_date)
 });
